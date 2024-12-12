805bfb711796370a623298b62518747eb17450d7
alperen tuydes <alperentuydes31@gmail.com>
2024-12-13 01:37:28 +0300

    GravityPro JS Project

---
+++ GravityPro/GravityPro/RealScript.js
@@ -0,0 +1,75 @@
+﻿const Container = document.querySelector(".container");
+const ContainerBody = document.body;
+const Gravity = 9.80665; // m/s^2
+const Mass = 10; // KG
+let Speed = 0;
+
+
+
+
+Container.addEventListener("click", (event) => {
+    if (event.target.className === Container.className) {
+
+        //console.log(`x position => ${event.clientX}`);
+        //console.log(`y position => ${event.clientY}`);
+        //console.log(`Gri kısmın ayarları; x => ${Container.offsetWidth}; y => ${Container.offsetHeight} `)
+
+        FuncCreateElement(event.clientX, event.clientY);
+
+
+    }
+})
+
+FuncCreateElement = (Left, Bottom) => {
+    let CreatedDiv = document.createElement("div");
+    CreatedDiv.style.position = "absolute";
+    CreatedDiv.style.left = `${Left}px`;
+
+    let calculatedBottom = FuncCalculateBottom(Bottom);
+    if (calculatedBottom > 0) {
+        CreatedDiv.style.bottom = `${calculatedBottom}px`;
+    } else {
+        CreatedDiv.style.bottom = `0px`;
+        console.log("Div yere ulaştı.");
+    }
+
+    //CreatedDiv.style.backgroundColor = "red";
+    CreatedDiv.style.backgroundColor = `rgb(${Math.ceil(Math.random() * 255)}, ${Math.ceil(Math.random() * 255)}, ${Math.ceil(Math.random() * 255)})`;
+    CreatedDiv.style.width = "50px";
+    CreatedDiv.style.height = "50px";
+    CreatedDiv.style.transform = "translate(-30px, 0px)";
+
+    Container.appendChild(CreatedDiv);
+    //FallAnimation(CreatedDiv);
+    FallAnimation(CreatedDiv, calculatedBottom);
+};
+
+
+
+FuncCalculateBottom = (yPosition) => {
+    let ContainerHeight = Container.offsetHeight;
+    let MiddleToBottom = 14;
+    let Bottom = ContainerHeight - yPosition - MiddleToBottom;
+    return Bottom;
+}
+
+
+//function FallAnimation(CreatedElement) {
+//    CreatedElement.style.bottom = `${CreatedElement.style.bottom - 1}px`;
+//    window.requestAnimationFrame(FallAnimation);
+//}
+
+
+function FallAnimation(CreatedElement, initialBottom) {
+    let currentBottom = parseInt(CreatedElement.style.bottom); // Sayıya dönüştürmeye yarıyor 
+
+    if (currentBottom > 0) {
+        currentBottom -= 2;
+        CreatedElement.style.bottom = `${currentBottom}px`;
+        requestAnimationFrame(() => FallAnimation(CreatedElement, initialBottom));
+    } else {
+        CreatedElement.style.bottom = "0px";
+        console.log(`Div yere ulaştı. Left => ${CreatedElement.style.left}px, Bottom => ${CreatedElement.style.bottom}`);
+    }
+}
+
---
+++ GravityPro/GravityPro/Script.js
@@ -0,0 +1,71 @@
+﻿const Container = document.querySelector(".container");
+
+const ContainerBody = document.body;
+
+const Gravity = 9.80665; // m/s^2
+const Mass = 10; // KG
+
+let Speed = 0;
+
+Container.addEventListener("click", (event) => {
+    console.log(`x position => ${event.clientX}`);
+    console.log(`y position => ${event.clientY}`);
+    CreateElement(event.clientX, event.clientY);
+})
+
+//function CreateElement(xPosition, yPosition) {
+//    let elementAttr = `<div style="width: 50px; height: 50px; background-color: red; position: absolute; left: ${xPosition}px; top: ${yPosition}px; transform: translate(-50%, -50%);"></div>`
+//    Container.innerHTML += elementAttr;
+//    FindAboutBottom(yPosition);
+//}
+
+//function CreateElement(xPosition, yPosition) {
+//    let elementAttr = `<div style="width: 50px; height: 50px; background-color: red; position: absolute; left: ${xPosition}px; bottom: ${FindAboutBottom(yPosition)}px; transform: translate(-50%, 50%);"></div>`
+//    Container.innerHTML += elementAttr;
+//    CalculateFallTime(FindAboutBottom(yPosition));
+//}
+
+function CreateElement(xPosition, yPosition) {
+    //let elementAttr = `<div style="width: 50px; height: 50px; background-color: red; position: absolute; left: ${xPosition}px; bottom: ${FindAboutBottom(yPosition)}px; transform: translate(-50%, 50%);"></div>`
+    //Container.innerHTML += elementAttr;
+
+    let elementAttr = document.createElement("div");
+    elementAttr.style.width = "50px";
+    elementAttr.style.height = "50px";
+    elementAttr.style.backgroundColor = "red";
+    elementAttr.style.position = "absolute";
+    elementAttr.style.left = `${xPosition}px`;
+    elementAttr.style.bottom = `${FindAboutBottom(yPosition)}px`; // bottom'a göre yüksekliği buldu.
+    //elementAttr.style.bottom = `${CalculateHeight(FindAboutBottom(yPosition))}px`; // bottom'a göre yüksekliği buldu.
+    elementAttr.style.transform = " translate(-50%, 50%)";
+    Container.appendChild(elementAttr);
+
+    CalculateFallTime(FindAboutBottom(yPosition)); // Düşme zamanını hesaplıyor ??
+
+
+    CalculateHeight(FindAboutBottom(yPosition));
+}
+
+function FindAboutBottom(yPosition) { // Yüksekliği bottom'a göre buluyor
+    let BigContainerHeight = ContainerBody.offsetHeight;
+    let RealHeight = BigContainerHeight - yPosition;
+    //console.log(`Real Height => ${RealHeight}px`);
+    return RealHeight;
+}
+
+function CalculateFallTime(RealHeight) {// Zamanı hesaplıyor
+    let time = Math.sqrt((2 * RealHeight) / Gravity);
+    console.log(time);
+}
+
+
+function CalculateHeight(ElementAttrHeight) {
+    while (ElementAttrHeight > 0) {
+        //ElementAttrHeight--;
+        setInterval(() => {
+            ElementAttrHeight--;
+        }, 1000000)
+        //return ElementAttrHeight;
+        console.log(ElementAttrHeight);
+    }
+}
